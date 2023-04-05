4b60d188a01f58087745f5b6fd7ce5c2db3bef55
arpitjaswal <iamarpitjaswal@gmail.com>
2023-04-05 23:43:48 +0530

    final commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const qr = document.getElementById("qrcode");
 
 const onGenerateSubmit = (e)=>{
     e.preventDefault();
-
+    clearUI();
     const url = document.getElementById("url").value;
     const size = document.getElementById("size").value;
 
@@ -17,19 +17,37 @@ const onGenerateSubmit = (e)=>{
         setTimeout(()=>{
             hideSpinner();
             generateQRCode(url,size);
-    }, 2000)
+            setTimeout(()=>{
+                const saveURL = document.querySelector("img").src;
+                createSaveButton(saveURL);
+            },50
+            )
+            document.getElementById("generated").appendChild(link);
+    }, 2000);
     }
 
 }
 
+
 const generateQRCode = (url, size) => {
-    const qrcode = new QRCode('qrcode', {
-      text: url,
-      width: size,
-      height: size,
+    var qrcode = new QRCode("qrcode", {
+        text: url,
+        width: size,
+        height: size,
     });
   };
 
+
+
+const createSaveButton = (saveURL)=>{
+    const link = document.createElement('a');
+    link.href = saveURL;
+    link.id = 'save-link';
+    link.innerHTML = "Save Button";
+    link.classList = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
+    link.download = "qrcode";
+    document.getElementById("generated").appendChild(link);
+}
 
 const showSpinner = ()=>{
     document.getElementById("spinner").style.display="block";
@@ -39,5 +57,10 @@ const hideSpinner = ()=>{
     document.getElementById("spinner").style.display="none";
 }
 
+const clearUI = ()=>{
+    qr.innerHTML = '';
+    const saveLink = document.getElementById("save-link");
+    if(saveLink)saveLink.remove();
+}
 
 form.addEventListener('submit', onGenerateSubmit);
